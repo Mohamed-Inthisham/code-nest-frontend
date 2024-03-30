d03f075b20fa0e4beaf20a4bcc704830b433af54
Mohamed-Inthisham <black.devil9b@gmail.com>
2024-03-30 07:40:03 +0530

    resources and roadmap crd

--- src/pages/ManageRoadmaps.jsx
+++ src/pages/ManageRoadmaps.jsx
@@ -1,167 +1,97 @@
-import React from 'react'
-import CompanySidebar from '../components/CompanySidebar'
-import '../css/ManageRoadmaps.css'
-import MentorSidebar from '../components/MentorSidebar'
+import React, { useState, useEffect } from 'react';
+import axiosInstance from '../api/axios';
+import MentorSidebar from '../components/MentorSidebar';
+import '../css/ManageRoadmaps.css';
 
 function ManageRoadmaps() {
+  const [roadmaps, setRoadmaps] = useState([]);
+
+  useEffect(() => {
+    fetchRoadmaps();
+  }, []);
+
+  const fetchRoadmaps = async () => {
+    try {
+      const response = await axiosInstance.get('/roadmaps');
+      setRoadmaps(response.data);
+    } catch (error) {
+      console.error('Error fetching roadmaps:', error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await axiosInstance.delete(`/roadmaps/${id}`);
+      // Update roadmaps state after deletion
+      const updatedRoadmaps = roadmaps.filter((roadmap) => roadmap.id !== id);
+      setRoadmaps(updatedRoadmaps);
+    } catch (error) {
+      console.error('Error deleting roadmap:', error);
+    }
+  };
+
   return (
-    
-    <div class="d-inline-flex p-2 bd-highlight">
-
-        <MentorSidebar />
-        <h1 className='text-one coursetitle'>Manage Roadmaps</h1>
-
-        <div className='armv'>
-        <button type="button" class="btn btn-primary arm"><a className='roadmapStyle' href='/addRoadmap'>Add Roadmap</a></button>
+    <div className="d-inline-flex p-2 bd-highlight">
+      <MentorSidebar />
+      <h1 className="text-one coursetitle">Manage Roadmaps</h1>
+      <div className="armv">
+        <button type="button" className="btn btn-primary arm">
+          <a className="roadmapStyle" href="/addRoadmap">
+            Add Roadmap
+          </a>
+        </button>
+      </div>
+      <div>
+        <div className="card managecourse">
+          <div className="table-responsive text-nowrap">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>
+                    <h4>Roadmap Name</h4>
+                  </th>
+                  <th>
+                    <h4>Mentor</h4>
+                  </th>
+                  <th>
+                    <h4>Action</h4>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="table-border-bottom-0">
+                {roadmaps.map((roadmap) => (
+                  <tr key={roadmap.id}>
+                    <td>
+                      <i className="bx  bx-sm me-3"></i>
+                      <span className="textv">{roadmap.rmTitle}</span>
+                    </td>
+                    <td>
+                      <div className="manageroadmap">
+                        <h5 type="text" className="manageroadmaptxt">
+                          {roadmap.mentorName}
+                        </h5>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="manageroadmap">
+                        <button
+                          type="button"
+                          className="manageroadmapbtn1"
+                          onClick={() => handleDelete(roadmap.id)}
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-
-        <div>
-
-        <div class="card managecourse">
-                
-                <div class="table-responsive text-nowrap">
-                  <table class="table">
-                    <thead>
-                      <tr>
-                        <th><h4>Roadmap Name</h4></th>
-                        <th><h4>Mentor</h4></th>
-                        <th><h4>Action</h4></th>
-                        
-                      </tr>
-                    </thead>
-                    <tbody class="table-border-bottom-0">
-                      <tr>
-                        <td>
-                          <i class="bx  bx-sm me-3"></i>
-                          <span class="textv">Java Road Map</span>
-                        </td>
-                        
-                        <td>
-                          <div class="manageroadmap">
-                            <h5 type="text" class="manageroadmaptxt" >
-                              K.L Santhush
-                            </h5>
-                            
-                             </div>
-                        </td>
-
-                        <td>
-                          <div class="manageroadmap">
-                            <button type="button" class="manageroadmapbtn1" >
-                              Delete
-                            </button>
-                             </div>
-                        </td>
-                      </tr>
-
-
-                      <tr>
-                        <td>
-                          <i class="bx  bx-sm me-3"></i>
-                          <span class="textv">.Net Road Map</span>
-                        </td>
-                        
-                        <td>
-                          <div class="manageroadmap">
-                            <h5 type="text" class="manageroadmaptxt" >
-                              A.H Divya
-                            </h5>
-                            
-                             </div>
-                        </td>
-
-                        <td>
-                          <div class="manageroadmap">
-                            <button type="button" class="manageroadmapbtn1" >
-                              Delete
-                            </button>
-                             </div>
-                        </td>
-                      </tr>
-
-                      <tr>
-                        <td>
-                          <i class="bx  bx-sm me-3"></i>
-                          <span class="textv">Java Operators</span>
-                        </td>
-                        
-                        <td>
-                          <div class="manageroadmap">
-                            <h5 type="text" class="manageroadmaptxt" >
-                              M.M Fernando
-                            </h5>
-                            
-                             </div>
-                        </td>
-
-                        <td>
-                          <div class="manageroadmap">
-                            <button type="button" class="manageroadmapbtn1" >
-                              Delete
-                            </button>
-                             </div>
-                        </td>
-                      </tr>
-
-                      <tr>
-                        <td>
-                          <i class="bx  bx-sm me-3"></i>
-                          <span class="textv">Frontend Developer</span>
-                        </td>
-                        
-                        <td>
-                          <div class="manageroadmap">
-                            <h5 type="text" class="manageroadmaptxt" >
-                              B.P.A Nora
-                            </h5>
-                            
-                             </div>
-                        </td>
-
-                        <td>
-                          <div class="manageroadmap">
-                            <button type="button" class="manageroadmapbtn1" >
-                              Delete
-                            </button>
-                             </div>
-                        </td>
-                      </tr>
-
-                      
-
-                      
-                      <tr>
-                        <td>
-                          <i class="bx  bx-sm me-3"></i>
-                          <span class="textv">Java Script Road Map</span>
-                        </td>
-
-                        <td>
-                          <div class="manageroadmap">
-                            <h5 type="text" class="manageroadmaptxt" >
-                              N.Y Perera
-                            </h5>
-                            
-                             </div>
-                        </td>
-                        
-                        <td>
-                          <div class="manageroadmap">
-                            <button type="button" class="manageroadmapbtn1" >
-                              Delete
-                            </button>
-                             </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-        </div>
-        
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default ManageRoadmaps
+export default ManageRoadmaps;
